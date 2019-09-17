df7fa4a2b8357f44d5a035b2b054c743693a5d9c
Abby <ahmchua@gmail.com>
2019-09-16 22:45:19 -0500

    initial commit

---
+++ project/__init__.py
@@ -0,0 +1,18 @@
+import torch
+    
+def superresolve(img, seed=2019):
+    """
+    YOUR CODE HERE
+    
+    Superresolve an image given the factor
+    @img: A numpy float array
+    @return A numpy float array
+    
+    e.g:
+    >>> img.shape
+    >>> (64,64,3)
+    >>> img = superresolve(img, 4)
+    >>> (256,256,3)
+    """
+    
+    raise NotImplemented("superresolve")
