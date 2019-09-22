e9f4e2b5dacf9c92009332b08579cfab92e3601a
Abby <ahmchua@gmail.com>
2019-09-22 14:08:25 -0500

    Defined custom dataset for coco

---
+++ project/data.py
@@ -0,0 +1,50 @@
+#from torchvision.vision import VisionDataset
+from torch.utils.data import Dataset
+from PIL import Image
+import os
+import os.path
+
+class MyCoco(Dataset):
+    """`MS Coco Detection <http://mscoco.org/dataset/#detections-challenge2016>`_ Dataset.
+
+    Args:
+        root (string): Root directory where images are downloaded to.
+        annFile (string): Path to json annotation file.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+        transforms (callable, optional): A function/transform that takes input sample and its target as entry
+            and returns a transformed version.
+    """
+
+    def __init__(self, root, annFile, transform=None, target_transform=None, transforms=None):
+        super(MyCoco, self).__init__()
+        from pycocotools.coco import COCO
+        self.coco = COCO(annFile)
+        self.ids = list(sorted(self.coco.imgs.keys()))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
+        """
+        coco = self.coco
+        img_id = self.ids[index]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        target = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(img_id)[0]['file_name']
+
+        img = Image.open(os.path.join(self.root, path)).convert('RGB')
+        small_img = img
+        if self.transforms is not None:
+            small_img = self.transforms(img)
+
+        return small_img, img
+
+    def __len__(self):
+        return len(self.ids)
